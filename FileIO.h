7b5f4561148cca242092edcc82d1b91d859d32d4
Naz Haque <naz1@me.com>
2014-09-28 20:49:18 +0100

    added make border header

--- FileIO.h
+++ FileIO.h
@@ -10,5 +10,7 @@ void getGameWidth(FILE *input, int *width);
 //Function which gets width of game board
 char **createBoard (int height, int width);
 //This function creates a dynamic 2D array using the user-defined size.
+char createBorder (char **board, int height, int width);
+//Creates a border around the user-defined gameboard so we can create a torroidal array
 void copyBoard (FILE *input, char **fileBoard, int height, int width);
 //This function copies the board from the file into the array
